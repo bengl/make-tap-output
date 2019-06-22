fd9e0c9514b2ae719292df29777e3ee387bb331d
Bryan English <bryan@bryanenglish.com>
2019-06-22 01:16:27 -0700

    modernize code

--- index.js
+++ index.js
@@ -3,125 +3,119 @@ Copyright 2015, Yahoo Inc. All rights reserved.
 Code licensed under the MIT License.
 See LICENSE.txt
 */
-var yaml = require('js-yaml');
-var Transform = require('readable-stream').Transform;
+const yaml = require('js-yaml');
+const { Transform } = require('readable-stream');
 
-var BAIL = 'Bail out!';
+const BAIL = 'Bail out!';
 
-function MakeTap (opts) {
-  Transform.call(this);
-  opts = opts || {};
-  this.opts = opts;
-  this.countEnabled = typeof opts.count !== 'boolean' ? true : opts.count;
-  if (!opts.noversion) {
-    this.writeln('TAP version 13');
+class MakeTap extends Transform {
+  constructor (opts) {
+    super();
+    opts = opts || {};
+    this.opts = opts;
+    this.countEnabled = typeof opts.count !== 'boolean' ? true : opts.count;
+    if (!opts.noversion) {
+      this.writeln('TAP version 13');
+    }
+    this.count = 0;
   }
-  this.count = 0;
+
+  _transform (data, encoding, callback) {
+    callback(null, data);
+  }
+
+  writeln (data) {
+    this.write(data + '\n');
+  }
+
+  diag (data) {
+    this.writeln(data.split('\n').map(l => `# ${l}`).join('\n'));
+  }
+
+  plan (num) {
+    this.writeln('1..' + num);
+  }
+
+  pass (message, directive) {
+    this.result({ message: message, ok: true, directive: directive });
+  }
+
+  fail (message, directive, err) {
+    if (arguments.length === 2 && typeof directive !== 'string') {
+      err = directive;
+      directive = undefined;
+    }
+    if (arguments.length === 1 && typeof message !== 'string') {
+      err = message;
+      message = undefined;
+      directive = undefined;
+    }
+    this.result({ message: message, directive: directive });
+    this.yaml(processError(err));
+  }
+
+  bail (message) {
+    this.writeln(message && message.length ? BAIL + ' ' + message : BAIL);
+  }
+
+  result (result) {
+    const line = [(result.ok ? 'ok' : 'not ok')];
+    if (this.countEnabled) line.push(++this.count);
+    if (result.message) line.push(result.message);
+    if (result.directive) line.push('# ' + result.directive);
+    this.writeln(line.join(' '));
+    if (typeof result.dataObj === 'object') {
+      this.yaml(result.dataObj);
+    }
+  }
+
+  yaml (data) {
+    this.writeln('  ---');
+    this.writeln(
+      yaml.safeDump(data)
+        .split('\n')
+        .map(l => l.length ? '  ' + l : '')
+        .join('\n')
+    );
+    this.writeln('  ...');
+  }
+
+  unbufferedSub (name) {
+    this.writeln('# Subtest: ' + name);
+    const opts = {};
+    for (const prop in this.opts) {
+      opts[prop] = this.opts[prop];
+    }
+    opts.noversion = true;
+    const subtap = new MakeTap(opts);
+    subtap.writeln = data =>
+      this.writeln(data.split('\n').map(l => '    ' + l).join('\n'));
+    return subtap;
+  }
+
+  bufferedSub (result) {
+    const line = [(result.ok ? 'ok' : 'not ok')];
+    if (this.countEnabled) line.push(++this.count);
+    if (result.message) line.push(result.message);
+    if (result.directive) line.push('# ' + result.directive);
+    this.writeln(line.join(' ') + ' {');
+    const opts = {};
+    for (const prop in this.opts) {
+      opts[prop] = this.opts[prop];
+    }
+    opts.noversion = true;
+    const subtap = new MakeTap(opts);
+    subtap.writeln = data =>
+      this.writeln(data.split('\n').map(l => '    ' + l).join('\n'));
+    subtap.end = () => this.writeln('}');
+    return subtap;
+  }
 }
-require('util').inherits(MakeTap, Transform);
 
-MakeTap.prototype._transform = function (data, encoding, callback) {
-  callback(null, data);
-};
-
-MakeTap.prototype.writeln = function (data) {
-  this.write(data + '\n');
-};
-
-MakeTap.prototype.diag = function (data) {
-  this.writeln(data.split('\n').map(function (l) {
-    return '# ' + l;
-  }).join('\n'));
-};
-
-MakeTap.prototype.plan = function (num) {
-  this.writeln('1..' + num);
-};
-
-MakeTap.prototype.pass = function (message, directive) {
-  this.result({ message: message, ok: true, directive: directive });
-};
-
-MakeTap.prototype.fail = function (message, directive, err) {
-  if (arguments.length === 2 && typeof directive !== 'string') {
-    err = directive;
-    directive = undefined;
-  }
-  if (arguments.length === 1 && typeof message !== 'string') {
-    err = message;
-    message = undefined;
-    directive = undefined;
-  }
-  this.result({ message: message, directive: directive });
-  this.yaml(processError(err));
-};
-
-MakeTap.prototype.bail = function (message) {
-  this.writeln(message && message.length ? BAIL + ' ' + message : BAIL);
-};
-
-MakeTap.prototype.result = function (result) {
-  var line = [(result.ok ? 'ok' : 'not ok')];
-  if (this.countEnabled) line.push(++this.count);
-  if (result.message) line.push(result.message);
-  if (result.directive) line.push('# ' + result.directive);
-  this.writeln(line.join(' '));
-  if (typeof result.dataObj === 'object') {
-    this.yaml(result.dataObj);
-  }
-};
-
-MakeTap.prototype.yaml = function (data) {
-  this.writeln('  ---');
-  this.writeln(yaml.safeDump(data).split('\n').map(function (l) {
-    return l.length ? '  ' + l : '';
-  }).join('\n'));
-  this.writeln('  ...');
-};
-
-MakeTap.prototype.unbufferedSub = function (name) {
-  this.writeln('# Subtest: ' + name);
-  var opts = {};
-  for (var prop in this.opts) {
-    opts[prop] = this.opts[prop];
-  }
-  opts.noversion = true;
-  var subtap = new MakeTap(opts);
-  var thisWriteln = this.writeln.bind(this);
-  subtap.writeln = function (data) {
-    thisWriteln(data.split('\n').map(l => '    ' + l).join('\n'));
-  };
-  return subtap;
-};
-
-MakeTap.prototype.bufferedSub = function (result) {
-  var line = [(result.ok ? 'ok' : 'not ok')];
-  if (this.countEnabled) line.push(++this.count);
-  if (result.message) line.push(result.message);
-  if (result.directive) line.push('# ' + result.directive);
-  this.writeln(line.join(' ') + ' {');
-  var opts = {};
-  for (var prop in this.opts) {
-    opts[prop] = this.opts[prop];
-  }
-  opts.noversion = true;
-  var subtap = new MakeTap(opts);
-  var thisWriteln = this.writeln.bind(this);
-  subtap.writeln = function (data) {
-    thisWriteln(data.split('\n').map(l => '    ' + l).join('\n'));
-  };
-  subtap.end = function () {
-    thisWriteln('}');
-  };
-  return subtap;
-};
-
-module.exports = function (opts) {
-  return new MakeTap(opts);
-};
+module.exports = opts => new MakeTap(opts);
 
 function processError (err) {
-  var obj = {};
+  const obj = {};
   applyProp(err, obj, 'name');
   applyProp(err, obj, 'message');
   if (err.name && err.name.indexOf('AssertionError') === 0) {
@@ -130,7 +124,7 @@ function processError (err) {
     applyProp(err, obj, 'actual');
   }
   if (err.stack) {
-    var stack = err.stack.split('\n');
+    const stack = err.stack.split('\n');
     stack.shift();
     obj.stack = stack.map(line => line.replace('    at ', '')).join('\n');
   }
--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
-var makeTap = require('../index');
-var fs = require('fs');
-var assert = require('assert');
+const makeTap = require('../index');
+const fs = require('fs');
+const assert = require('assert');
 
-var testOutput = fs.readFileSync(require('path').join(__dirname, 'test1.tap'), 'utf8');
-var buff = '';
-var exitCode = 0;
+let testOutput = fs.readFileSync(require('path').join(__dirname, 'test1.tap'), 'utf8');
+let buff = '';
+let exitCode = 0;
 
-var testErr = {
+const testErr = {
   name: 'Error',
   message: 'foobar',
   stack: 'Error: foobar\n    at line1\n    at line2'
 };
 
-var testAssertionError = {
+const testAssertionError = {
   name: 'AssertionError',
   message: 'foomessage',
   operator: '==',
@@ -21,7 +21,7 @@ var testAssertionError = {
   stack: 'AssertionError: foomessage\n    at line1\n    at line2'
 };
 
-var testTap = makeTap();
+const testTap = makeTap();
 
 testTap.writeln('# foo');
 
@@ -50,21 +50,21 @@ testTap.yaml({
   stuff: 1
 });
 
-var sub1 = testTap.unbufferedSub('sub1');
+const sub1 = testTap.unbufferedSub('sub1');
 sub1.plan(3);
 sub1.pass('a pass');
 sub1.fail('a fail', testErr);
-var subsub1 = sub1.unbufferedSub('subsub1');
+const subsub1 = sub1.unbufferedSub('subsub1');
 subsub1.plan(1);
 subsub1.pass('a pass');
 sub1.pass('subsub1');
 testTap.pass('sub1');
 
-var sub2 = testTap.bufferedSub({ ok: true, message: 'sub2' });
+const sub2 = testTap.bufferedSub({ ok: true, message: 'sub2' });
 sub1.plan(2);
 sub2.pass('a pass');
 sub2.fail('a fail', testErr);
-var subsub2 = sub2.bufferedSub({ ok: true, message: 'subsub2' });
+const subsub2 = sub2.bufferedSub({ ok: true, message: 'subsub2' });
 subsub2.plan(1);
 subsub2.pass('a pass');
 subsub2.end();
@@ -73,7 +73,7 @@ sub2.end();
 
 testTap.end();
 
-var testTap2 = makeTap({ count: false });
+const testTap2 = makeTap({ count: false });
 
 testTap2.pass();
 testTap2.pass('foo');
@@ -84,7 +84,7 @@ testTapPipe(testTap);
 testTapPipe(testTap2);
 testTap2.on('end', function () {
   process.nextTick(function () {
-    var mainTap = makeTap();
+    const mainTap = makeTap();
     testOutput = testOutput.split('\n');
     buff = buff.split('\n');
     mainTap.plan(testOutput.length);
@@ -107,7 +107,7 @@ function testTapPipe (tap) {
 }
 
 function assertEqual (tap, x, y) {
-  var name = '"' + x + '" === "' + y + '"';
+  const name = '"' + x + '" === "' + y + '"';
   try {
     assert.strictEqual(x, y);
     tap.pass(name);
